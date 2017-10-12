8c412b00bd61b31cc15aeb927c8622ddb8bd45fd
enzoftware <lizama.enzo@gmail.com>
2017-10-11 22:53:54 -0500

    add some kotlin style

--- Android Basics-User Interface/app/src/main/java/com/udc/enzoftware/birthdaycard/MainActivity.kt
+++ Android Basics-User Interface/app/src/main/java/com/udc/enzoftware/birthdaycard/MainActivity.kt
@@ -2,11 +2,27 @@ package com.udc.enzoftware.birthdaycard
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var text = true
+        val description = resources.getString(R.string.description )
+        val name = resources.getString(R.string.name)
+        dynamicText.setOnClickListener {
+            if(text){
+                dynamicText.text = description
+                dynamicText.textSize = 20.toFloat()
+                text = false
+            }else{
+                dynamicText.text = name
+                dynamicText.textSize = 29.toFloat()
+                text = true
+            }
+
+        }
     }
 }
