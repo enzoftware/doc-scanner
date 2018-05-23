0e6a697283e0f8784511f6c69d7fefa13bc68511
enzoftware <lizama.enzo@gmail.com>
2018-05-23 00:33:52 -0500

    Finish doc reader :snake:

--- scan.py
+++ scan.py
@@ -1,6 +1,5 @@
 from pyimagesearch.transform import four_point_transform
 from skimage.filters import threshold_local
-import numpy as np
 import argparse
 import cv2
 import imutils
@@ -25,5 +24,30 @@ cv2.imshow("Edged", edged)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:5]
+for c in cnts:
+    peri = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, 0.02 * peri, True)
 
+    if len(approx) == 4:
+        screenCnt = approx
+        break
 
+print("STEP 2: FIND CONTOURS OF PAPER")
+cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
+cv2.imshow("Outline", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+warped = four_point_transform(orig, screenCnt.reshape(4, 2) * ratio)
+
+warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
+t = threshold_local(warped, 11, offset=10, method="gaussian")
+warped = (warped > t).astype("uint8") * 255
+
+print("STEP 3: Apply perspective transform")
+cv2.imshow("Original", imutils.resize(orig, height = 650))
+cv2.imshow("Scanned", imutils.resize(warped, height = 650))
+cv2.waitKey(0)
